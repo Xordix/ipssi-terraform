d0a52f70ee8df35bc6c91d62f372129722729e87
Ysalai <ysalai@deliverup.com>
2026-04-16 16:32:59 +0200

    feat: uncomment missing locals var

--- terraform-tp-grp/tp05-starter/modules/compute/locals.tf
+++ terraform-tp-grp/tp05-starter/modules/compute/locals.tf
@@ -2,8 +2,8 @@ locals {
   name_prefix = "${var.project_name}-${var.environment}"
 
   # Transformation map -> list pour les ressources qui attendent une liste
-  # public_subnet_ids_list      = values(var.public_subnet_ids)
-  # private_app_subnet_ids_list = values(var.private_app_subnet_ids)
+  public_subnet_ids_list      = values(var.public_subnet_ids)
+  private_app_subnet_ids_list = values(var.private_app_subnet_ids)
 
   common_tags = {
     Project     = var.project_name
