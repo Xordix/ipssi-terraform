90fc45856d3dba726765c9eb575d02c22653a895
Ben-Ayoub <bentoumia.ayoub13@gmail.com>
2026-04-16 16:11:16 +0200

    variables.tf

--- terraform-tp-grp/tp05-starter/modules/data/variables.tf
+++ terraform-tp-grp/tp05-starter/modules/data/variables.tf
@@ -1,60 +1,55 @@
 variable "vpc_id" {
-  description = "ID du VPC (output du module networking)."
   type        = string
+  description = "ID du VPC (output du module networking)"
 }
 
 variable "private_db_subnet_ids" {
-  description = "Map AZ -> subnet_id prive DB (2 subnets sur 2 AZ pour Multi-AZ)."
   type        = map(string)
+  description = "Map AZ -> subnet_id prive DB (2 subnets sur 2 AZ pour Multi-AZ)"
 }
 
 variable "db_security_group_id" {
-  description = "SG attache au RDS (fourni par le module security)."
   type        = string
+  description = "SG attache au RDS (fourni par le module security)"
 }
 
 variable "kms_key_arn" {
-  description = "ARN de la CMK KMS (fournie par le module security)."
   type        = string
+  description = "ARN de la CMK KMS (fournie par le module security)"
 }
 
 variable "db_password_secret_arn" {
-  description = "ARN du secret Secrets Manager contenant le mot de passe DB."
   type        = string
+  description = "ARN du secret Secrets Manager contenant le mot de passe DB"
 }
 
 variable "project_name" {
-  description = "Nom de projet pour le tagging."
-  type        = string
-  default     = "kolab"
+  type    = string
+  default = "kolab"
 }
 
 variable "environment" {
-  description = "Nom de l environnement."
-  type        = string
-  default     = "dev"
+  type    = string
+  default = "dev"
 }
 
 variable "db_instance_class" {
-  description = "Classe d instance RDS. db.t3.micro = plus petit dispo Multi-AZ."
-  type        = string
-  default     = "db.t3.micro"
+  type    = string
+  default = "db.t3.micro"
 }
 
 variable "db_allocated_storage" {
-  description = "Storage initial RDS en Go."
-  type        = number
-  default     = 20
+  type    = number
+  default = 20
 }
 
 variable "db_max_allocated_storage" {
-  description = "Storage max (auto-scaling gp3) pour absorber les uploads Nextcloud."
   type        = number
   default     = 100
+  description = "Autoscaling storage (gp3) pour absorber les uploads Nextcloud"
 }
 
 variable "db_engine_version" {
-  description = "Version PostgreSQL."
-  type        = string
-  default     = "16.4"
+  type    = string
+  default = "16.4"
 }
